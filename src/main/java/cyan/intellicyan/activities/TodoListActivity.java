f1dbe556ec70aa1033ba5b97b2050d087497b54b
Cyan's <dearse7en@gmail.com>
2017-03-21 23:05:14 +0800

    by cyan

--- src/main/java/cyan/intellicyan/activities/TodoListActivity.java
+++ src/main/java/cyan/intellicyan/activities/TodoListActivity.java
@@ -1,8 +1,11 @@
 package cyan.intellicyan.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import cyan.intellicyan.R;
 import cyan.intellicyan.activities.base.BaseCompatActivity;
@@ -12,6 +15,8 @@ public class TodoListActivity extends BaseCompatActivity {
     public static final String EXTRA_DATE_M = "extra_date_m";
     public static final String EXTRA_DATE_D = "extra_date_d";
 
+    public FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,13 @@ public class TodoListActivity extends BaseCompatActivity {
         getToolbarTitle().setText("主界面");
        //getSubTitle().setText("更多");
 
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.todo_add_button);
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(TodoListActivity.this,"???",Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     @Override
